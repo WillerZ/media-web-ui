27563cbb8ab891d14ad91e6eaed76db2681de0c4
Phil Willoughby <willerz@gmail.com>
2021-10-31 13:35:55 -0700

    Move to next item in folder when playback ends

--- pages/[[...media]].tsx
+++ pages/[[...media]].tsx
@@ -7,15 +7,18 @@ import path from 'path';
 import { MediaDir, MediaExtensions, AudioExtensions, SkipFolders } from '../lib/constants';
 import { trackStringCompare } from "../lib/track";
 import assert from "assert";
+import { useRouter } from "next/dist/client/router";
 
 type MediaProps = {
     path: string[],
     MediaElement?: 'video' | 'audio',
     mediaProps?: object,
-    subpaths?: string[]
+    subpaths?: string[],
+    nextPage?: string
 };
 
-const Media: NextPage<MediaProps> = ({ path, subpaths, MediaElement, mediaProps }: MediaProps) => {
+const Media: NextPage<MediaProps> = ({ path, subpaths, MediaElement, mediaProps, nextPage }: MediaProps) => {
+    const router = useRouter();
     const titleText = (path.length > 0) ? path.join(' > ') : 'Home';
     if (Array.isArray(subpaths)) {
         return (
@@ -30,13 +33,15 @@ const Media: NextPage<MediaProps> = ({ path, subpaths, MediaElement, mediaProps 
             </>
         );
     } else {
+        let chainMedia = true;
         assert(MediaElement !== undefined);
+        const onEnded = nextPage ? () => { if (chainMedia) { router.push(nextPage); } } : undefined;
         return (
             <>
                 <Head>
                     <title>{titleText}</title>
                 </Head>
-                <MediaElement controls autoPlay src={['', ...path].map(encodeURIComponent).join('/')} {...mediaProps} />
+                <MediaElement controls autoPlay onEnded={onEnded} src={['', ...path].map(encodeURIComponent).join('/')} {...mediaProps} />
                 <h1>
                     <LinkTrails path={path} />
                 </h1>
@@ -70,7 +75,20 @@ export const getStaticProps: GetStaticProps = async (context) => {
         }
         assert(!!media);
         const MediaElement = AudioExtensions.includes(path.extname(media[media.length - 1])) ? 'audio' : 'video';
-        return { props: { path: media, MediaElement }, revalidate: 120 };
+        let nextPage;
+        try {
+            const contents = (await fs.readdir(path.dirname(mediaPath), { withFileTypes: true }))
+                .filter(dent => !dent.isDirectory() && MediaExtensions.includes(path.extname(dent.name)))
+                .map(dent => dent.name)
+                .sort(trackStringCompare);
+            const thisPageIndex = contents.indexOf(media[media.length - 1]);
+            if (thisPageIndex > 0 && thisPageIndex < media.length - 1) {
+                nextPage = ['', ...media.slice(0, media.length -1), contents[thisPageIndex + 1] + '.html'].join('/');
+            }
+        } catch (e2) {
+            // Disregard, chaining is not critical
+        }
+        return { props: { path: media, MediaElement, nextPage: nextPage || null }, revalidate: 120 };
     }
 };
 
